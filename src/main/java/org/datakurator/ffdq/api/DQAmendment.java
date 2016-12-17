b79ac562c0dd1e3ce9dbe4276181d87d28a04290
Paul J. Morris <mole@morris.net>
2016-12-17 14:12:33 -0500

    ISSUE: PURPOSE: Fix build. DESCRIPTION: Cleaining up syntax in a comment.

--- src/main/java/org/datakurator/ffdq/api/DQAmendment.java
+++ src/main/java/org/datakurator/ffdq/api/DQAmendment.java
@@ -27,15 +27,16 @@ public interface DQAmendment {
 	/**
 	 * Obtain the result state (completed or some problem condition) of the amendment.
 	 * 
-	 * @return the result state of the test that attempted to assert an amendment, DQResultState.COMPLETED if successful.
-	 * @see DQResultState
+	 * @return the result state of the test that attempted to assert an amendment, EnumDQResultState.COMPLETED if successful.
+	 * 
+	 * @see org.datakurator.ffdq.api.EnumDQResultState
 	 */
-    QCResultState getResultState();
+    EnumDQResultState getResultState();
     /**
      * Obtain a map of keys and values for the terms and new values proposed by the amendment.
      * 
      * @param key ? key to check for ?
-     * @return a Map<key, value> of the new values and the terms they are proposed to apply to.
+     * @return a Map key, value of the new values and the terms they are proposed to apply to.
      */
     Map<String, String> getResult(String key);
     /**
