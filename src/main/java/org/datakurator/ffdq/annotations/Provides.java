a7263f464a8393fd587da1836a718f76a223cf38
chicoreus <mole@morris.net>
2024-10-25 19:10:30 -0400

    Adding comments describing the expected content of the Provides and ProvidesVersion annotations.

--- src/main/java/org/datakurator/ffdq/annotations/Provides.java
+++ src/main/java/org/datakurator/ffdq/annotations/Provides.java
@@ -6,6 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * In the @Provides(value) annotation, the value is the term_localName 
+ * for a test that is implemented by the annotated method.
+ * 
  * Created by lowery on 12/14/16.
  */
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/org/datakurator/ffdq/annotations/ProvidesVersion.java
+++ src/main/java/org/datakurator/ffdq/annotations/ProvidesVersion.java
@@ -6,6 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * In the @ProvidesVersion(value) annotation, the value is the Term IRI  
+ * for a test that is implemented by the annotated method suffixed by 
+ * a / and the DateLastModified for the test.  Note that this is 
+ * different from the IRI, where the Term IRI is suffixed by a - and 
+ * the issued date.
+ * 
  * Created by Paul J. Morris on 2023-06-05.
  */
 @Retention(RetentionPolicy.RUNTIME)
